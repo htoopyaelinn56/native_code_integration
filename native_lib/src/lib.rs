97b9f939b7a70350e61ec617130c9ae1907d082e
htoopyaelinn56 <hplinn0@gmail.com>
2025-07-12 20:44:31 +0630

    add async functionality to native functions and integrate random data fetching

--- native_lib/src/lib.rs
+++ native_lib/src/lib.rs
@@ -1,6 +1,7 @@
 mod native_functions;
 
 use std::ffi::{c_char, CString};
+use tokio::runtime::Runtime;
 
 #[no_mangle]
 pub extern "C" fn add_ffi(left: u64, right: u64) -> u64 {
@@ -11,4 +12,13 @@ pub extern "C" fn add_ffi(left: u64, right: u64) -> u64 {
 pub extern "C" fn greet_ffi() -> *mut c_char {
     let s = native_functions::greet();
     CString::new(s).unwrap().into_raw()
+}
+
+#[no_mangle]
+pub extern "C" fn get_random_ffi() -> *const c_char {
+    // Create a runtime for async execution
+    let rt = Runtime::new().unwrap();
+    let result = rt.block_on(native_functions::get_random());
+    let c_string = CString::new(result).unwrap();
+    c_string.into_raw()
 }
--- native_lib/src/native_functions.rs
+++ native_lib/src/native_functions.rs
@@ -6,13 +6,34 @@ pub fn greet() -> String {
     "Hello From Rust! 🦀".to_string()
 }
 
+pub async fn get_random() -> String {
+    let body = reqwest::get("https://bored-api.appbrewery.com/random")
+        .await
+        .unwrap()
+        .text()
+        .await;
+
+    let body = body.unwrap();
+    body
+}
+
 #[cfg(test)]
 mod tests {
     use super::*;
 
     #[test]
-    fn it_works() {
-        let result = add(2, 2);
-        assert_eq!(result, 4);
+    fn add_works() {
+        assert_eq!(add(3, 5), 8);
+    }
+
+    #[test]
+    fn greet_works() {
+        assert_eq!(greet(), "Hello From Rust!  🦀");
+    }
+
+    #[tokio::test]
+    async fn greet_async_works() {
+        let random_data = get_random().await;
+        println!("{}", random_data);
     }
 }
